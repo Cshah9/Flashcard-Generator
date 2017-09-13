f6523b82b85a04e9542535b6bbf41a2f88792984
Chirag K Shah <chiragkshah@Chirags-MBP.home>
2017-09-13 00:46:08 -0400

    add prototype functions to ClozeCard.js

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,15 +1,34 @@
 function ClozeCard(text, cloze) {
+	this.fullText;
+	this.cloze;
+	this.partial;
 
 	if ( text.includes(cloze) ) {
-		this.fullText = text;
-		this.cloze = cloze;	
-		this.partial = text.replace(cloze, " ... "); 
+
+
+		this.addFullText(text);
+		this.addCloze(cloze);
+		this.createPartial();	
 		
+
 	}
 	else {
 		console.log("Cannot create this card becuase '", cloze,"' doesn't appear in '", text, "'");
 	}
 }
 
+ClozeCard.prototype.addCloze = function(newCloze) {
+	this.cloze = newCloze;
+	console.log("--ClozeCard-- setting cloze:", this.cloze);
+}
+
+ClozeCard.prototype.addFullText = function(newText) {
+	this.fullText = newText;
+	console.log("--ClozeCard-- setting fullText:", this.fullText);
+}
+ClozeCard.prototype.createPartial = function() {
+	this.partial = this.fullText.replace(this.cloze, " ... "); 
+	console.log("--ClozeCard-- creating partial:", this.partial);
+}
 
 module.exports = ClozeCard;
--- testcards.js
+++ testcards.js
@@ -1,6 +1,8 @@
 var BasicCard = require('./BasicCard.js');
 var ClozeCard = require('./ClozeCard.js');
 
+console.log("\n");
+
 var firstPresident = new BasicCard(
     "Who was the first president of the United States?", "George Washington");
 
@@ -9,11 +11,11 @@ console.log(firstPresident.front);
 
 // "George Washington"
 console.log(firstPresident.back); 
-
+console.log("\n");
 
 var firstPresidentCloze = new ClozeCard(
     "George Washington was the first president of the United States.", "George Washington");
-
+console.log("\n");
 // "George Washington"
 console.log(firstPresidentCloze.cloze); 
 
